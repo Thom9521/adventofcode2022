022affde4121807386e37e25eb997cd0e68b870a
Thom9521 <thomas96mc@gmail.com>
2022-12-03 09:03:14 +0100

    day 1 and 2

---
+++ day1/part1.php
@@ -0,0 +1,21 @@
+<?php
+$data = file_get_contents("data.txt");
+
+$caloriesSeperatedByElf = explode("\r\n\r\n", $data);
+
+$caloriesArray = [];
+
+foreach ($caloriesSeperatedByElf as $elf) {
+    $caloriesAsArray = explode("\r", $elf);
+    $calories = 0;
+
+    foreach ($caloriesAsArray as $fruit) {
+        $calories += (int)$fruit;
+    }
+    $caloriesArray[] = $calories;
+}
+
+sort($caloriesArray, SORT_NUMERIC);
+$caloriesArray = array_reverse($caloriesArray);
+
+echo $caloriesArray[0];
