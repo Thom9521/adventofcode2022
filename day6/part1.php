7b6ac68ced9bbb21c76b62373fbf6fa709c11c1b
Thom9521 <thomas96mc@gmail.com>
2022-12-06 15:38:51 +0100

    day6

--- day6/part1.php
+++ day6/part1.php
@@ -2,43 +2,20 @@
 $data = file_get_contents(__DIR__ . "./data.txt");
 $dataAsArray = str_split($data);
 
-$char1 = "";
-$char2 = "";
-$char3 = "";
-$char4 = "";
+$charArray = [];
 
 $count = 0;
 foreach ($dataAsArray as $char) {
     $count++;
 
-    if (empty($char1)) {
-        $char1 = $char;
-    } else if (empty($char2)) {
-        $char2 = $char;
-    } else if (empty($char3)) {
-        $char3 = $char;
-    } else if (empty($char4)) {
-        $char4 = $char;
+    if (!in_array($char, $charArray)) {
+        $charArray[] = $char;
+    } else {
+        $charArray = [$char];
     }
 
-    if (!empty($char1) && !empty($char2) && !empty($char3) && !empty($char4)) {
-        if (
-            $char1 != $char2 &&
-            $char1 != $char3 &&
-            $char1 != $char4 &&
-            $char2 != $char3 &&
-            $char2 != $char4 &&
-            $char3 != $char4
-        ) {
-            echo $count;
-            break;
-        } else {
-            echo $char1 . " " . $char2 . " " . $char3 . " " . $char4;
-            echo PHP_EOL;
-            $char1 = "";
-            $char2 = "";
-            $char3 = "";
-            $char4 = "";
-        }
+    if (count($charArray) == 4) {
+        echo $count;
+        break;
     }
 }
