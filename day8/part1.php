5150cc342442c3081701436a8b7b91fb0d8247c5
Thom9521 <thomas96mc@gmail.com>
2022-12-09 08:27:07 +0100

    day8 more trash

--- day8/part1.php
+++ day8/part1.php
@@ -49,78 +49,105 @@ foreach ($dataAsArray as $index => $row) {
     $totalVisibleTrees += $visibleTrees;
 }
 
-// $dataIndex = count($dataAsArray);
-
-// while ($dataIndex) {
-//     $tree = $row[--$dataIndex];
-//     $treeComp = $dataIndex . "_" . $tree;
-//     echo $treeComp . PHP_EOL;
-//     if ($tree > $biggesTree && !in_array($treeComp, $treesFound)) {
-//         echo $treeComp . PHP_EOL;
-//         $treesFound[] = $treeComp;
-//         $visibleTrees++;
-//         $biggesTree = $tree;
-//     }
-// }
-// exit();
-
-/*
-$index2 = count(str_split(trim($row)));
-
-while ($index2) {
-    $tree = $row[--$index2];
-    $treeComp =  $index . "_" . $index2;
-    if ($tree > $biggesTree && !in_array($treeComp, $treesFound)) {
-        echo $treeComp . PHP_EOL;
-        $treesFound[] = $treeComp;
-        $visibleTrees++;
-        $biggesTree = $tree;
-    }
-}
-exit();
-*/
+$tmpSmallestTress = [];
 
 // From top
-foreach (str_split(trim($dataAsArray[0])) as $indexCol => $column) {
-
+foreach ($dataAsArray as $index => $row) {
+    $trees = str_split(trim($row));
     $visibleTrees = 0;
-    $biggesTree = -1;
 
-    foreach ($dataAsArray as $indexRow => $row) {
-        $trees = str_split(trim($row));
-        $treeComp = $colLength -  $indexCol . "_" . $indexRow;
-        if ($trees[$indexCol] > $biggesTree && !in_array($treeComp, $treesFound)) {
+
+    foreach ($trees as $index2 => $tree) {
+        $treeComp = $index . "_" . $index2;
+        // echo $treeComp . PHP_EOL;
+        if (!array_key_exists($index2, $tmpSmallestTress)) {
+            $tmpSmallestTress[$index2] = $tree;
             $treesFound[] = $treeComp;
             $visibleTrees++;
-            $biggesTree = $trees[$indexCol];
-        } else {
-            break;
+        } else if (array_key_exists($index2, $tmpSmallestTress)) {
+            if ($tmpSmallestTress[$index2] < $tree) {
+                // echo $treeComp . PHP_EOL;
+                $tmpSmallestTress[$index2] = $tree;
+                if (!in_array($treeComp, $treesFound)) {
+                    $treesFound[] = $treeComp;
+                    $visibleTrees++;
+                }
+            }
         }
     }
     $totalVisibleTrees += $visibleTrees;
 }
 
+
+$tmpSmallestTress = [];
 
 // From bottom
-foreach (str_split(trim($dataAsArray[0])) as $indexCol => $column) {
+foreach (array_reverse($dataAsArray) as $index => $row) {
+    $trees = str_split(trim($row));
     $visibleTrees = 0;
-    $biggesTree = -1;
-
-    foreach (array_reverse($dataAsArray) as $indexRow => $row) {
-        $trees = str_split(trim($row));
-        $treeComp = $colLength -  $indexCol . "_" . $rowLength - $indexRow;
-        if ($trees[$indexCol] > $biggesTree && !in_array($treeComp, $treesFound)) {
+    foreach ($trees as $index2 => $tree) {
+        $treeComp = $index . "_" . $index2;
+        echo $treeComp . PHP_EOL;
+        if (!array_key_exists($index2, $tmpSmallestTress)) {
+            $tmpSmallestTress[$index2] = $tree;
             $treesFound[] = $treeComp;
-            echo $treeComp . PHP_EOL;
-
             $visibleTrees++;
-            $biggesTree = $trees[$indexCol];
-        } else {
-            break;
+        } else if (array_key_exists($index2, $tmpSmallestTress)) {
+            if ($tmpSmallestTress[$index2] < $tree) {
+                echo $treeComp . PHP_EOL;
+                $tmpSmallestTress[$index2] = $tree;
+                if (!in_array($treeComp, $treesFound)) {
+                    $treesFound[] = $treeComp;
+                    $visibleTrees++;
+                }
+            }
         }
     }
     $totalVisibleTrees += $visibleTrees;
 }
-print_r($treesFound);
+echo count($treesFound) . "  " . $totalVisibleTrees;
+// // From top
+// foreach (str_split(trim($dataAsArray[0])) as $indexCol => $column) {
+
+//     $visibleTrees = 0;
+//     $biggesTree = -1;
+
+//     foreach ($dataAsArray as $indexRow => $row) {
+//         $trees = str_split(trim($row));
+//         $treeComp = $indexCol . "_" . $indexRow;
+//         if ($trees[$indexCol] > $biggesTree && !in_array($treeComp, $treesFound)) {
+//             $treesFound[] = $treeComp;
+//             $visibleTrees++;
+//             $biggesTree = $trees[$indexCol];
+//             echo $trees[$indexCol] . " " . $treeComp .  PHP_EOL;
+//         } else {
+//             continue;
+//         }
+//     }
+//     $totalVisibleTrees += $visibleTrees;
+// }
+
+
+// // From bottom
+// foreach (str_split(trim($dataAsArray[0])) as $indexCol => $column) {
+//     $visibleTrees = 0;
+//     $biggesTree = -1;
+
+//     foreach (array_reverse($dataAsArray) as $indexRow => $row) {
+//         $trees = str_split(trim($row));
+//         $treeComp = $colLength -  $indexCol . "_" . $rowLength - $indexRow;
+//         if ($trees[$indexCol] > $biggesTree && !in_array($treeComp, $treesFound)) {
+//             $treesFound[] = $treeComp;
+//             // echo $treeComp . PHP_EOL;
+
+//             $visibleTrees++;
+//             $biggesTree = $trees[$indexCol];
+//         } else {
+//             break;
+//         }
+//     }
+//     $totalVisibleTrees += $visibleTrees;
+// }
+// print_r($treesFound);
 // print_r(array_reverse($dataAsArray));
-echo PHP_EOL . $totalVisibleTrees;
+// echo PHP_EOL . $totalVisibleTrees;
